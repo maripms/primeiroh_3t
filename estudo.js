e39b5c2d3c1d9c59c012af3586ad91e869eadb99
mariane pacheco <106189970+maripms@users.noreply.github.com>
2022-09-27 16:56:08 -0300

    27/09

--- estudo.js
+++ estudo.js
@@ -1,1 +1,7 @@
+// alert("Oi, eu sou um alert!");
+// console.log("Oi, eu estou no console.")
+// document.write("Aula de Pens. Comp.");
 
+function meChame( ){
+    alert("Oi, eu sou um alert!");
+}
