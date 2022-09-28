1186b5b29e66ae8a7bbbf6284aa36b3f9618f821
mariane pacheco <106189970+maripms@users.noreply.github.com>
2022-09-28 18:33:03 -0300

    Update estudo.js

--- estudo.js
+++ estudo.js
@@ -2,6 +2,20 @@
 // console.log("Oi, eu estou no console.")
 // document.write("Aula de Pens. Comp.");
 
-function meChame( ){
-    alert("Oi, eu sou um alert!");
+let tabuada = 2;
+
+function meChame(Mariane){
+    document.write("<h1>Tabuada do " + tabuada + "</h1>");
+    document.write(tabuada +" x 1 = " + (tabuada*1)+"<br>");
+    document.write(tabuada +" x 2 = " + (tabuada*2)+"<br>");
+    document.write(tabuada +" x 3 = " + (tabuada*3)+"<br>");
+    document.write(tabuada +" x 4 = " + (tabuada*4)+"<br>");
+    document.write(tabuada +" x 5 = " + (tabuada*5)+"<br>");
+    document.write(tabuada +" x 6 = " + (tabuada*6)+"<br>");
+    document.write(tabuada +" x 7 = " + (tabuada*7)+"<br>");
+    document.write(tabuada +" x 8 = " + (tabuada*8)+"<br>");
+    document.write(tabuada +" x 9 = " + (tabuada*9)+"<br>");
+    document.write(tabuada +" x 10 = " + (tabuada*10)+"<br><br>");
+    document.write("Feita por " + Mariane);
 }
+
